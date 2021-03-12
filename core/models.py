ec9a2623bbc61f398a780c7758902ca99ddfb1a6
lcbiplove <lcbiplove1@gmail.com>
2021-03-12 13:52:17 +0545

    pp field added to user

--- core/models.py
+++ core/models.py
@@ -33,6 +33,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     """Custom user model that supports using email instead of username"""
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
+    pp = models.ImageField(upload_to='photos/pp')
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
--- nepfdb/settings.py
+++ nepfdb/settings.py
@@ -113,10 +113,10 @@ USE_L10N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.1/howto/static-files/
-
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = "core.User"
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
